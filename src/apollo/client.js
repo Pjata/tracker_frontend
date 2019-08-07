d64bbce38d6f2742be8aa29cfc61dc24603e4128
Pjatacsuk Bence <pjatacsuk@gmail.com>
2019-08-07 13:46:24 +0200

    apollo client

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,6 +1,9 @@
 import ApolloClient from "apollo-boost"
-
+const uri =
+  process.env.NODE_ENV === "production"
+    ? "https://trackergraphql.herokuapp.com/graphql"
+    : "http://localhost:4000/graphql"
 export const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql"
+  uri
 })
 export default client
--- src/index.js
+++ src/index.js
@@ -5,10 +5,8 @@ import App from "./App"
 import * as serviceWorker from "./serviceWorker"
 import ApolloClient from "apollo-boost"
 import { ApolloProvider } from "@apollo/react-hooks"
+import client from "./apollo/client"
 
-const client = new ApolloClient({
-  uri: "http://localhost:4000/graphql"
-})
 ReactDOM.render(
   <ApolloProvider client={client}>
     <App />
