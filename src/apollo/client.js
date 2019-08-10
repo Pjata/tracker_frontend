9ccd2632d9f78fbe9e62451b71f0e254d18f80b1
Pjatacsuk Bence <pjatacsuk@gmail.com>
2019-08-10 10:28:12 +0200

    client

--- src/apollo/client.js
+++ src/apollo/client.js
@@ -1,6 +1,7 @@
 import ApolloClient from "apollo-boost"
+console.log(process.env.NODE_ENV)
 const uri =
-  process.env.NODE_ENV === "production"
+  process.env.NODE_ENV !== "development"
     ? "https://trackergraphql.herokuapp.com/graphql"
     : "http://localhost:4000/graphql"
 export const client = new ApolloClient({
